458d95215d31077f60d547f134ca61c061f4c902
Joscelyn <stonis.j@husky.neu.edu>
2016-02-27 11:17:55 -0500

    in process of assignment 3

---
+++ public/assignment/app.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Joscelyn on 2/27/2016.
+ */
+
+(function(){
+    angular.module("FormBuilderApp", ["ngRoute"])
+})();
---
+++ public/assignment/config.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Josceyn on 2/27/2016.
+ */
+(function(){
+    angular
+        .module("FormBuilderApp")
+        .config(function($routeProvider){
+            $routeProvider
+                .when("/", {
+                    templateUrl: "views/home/home.view.html",
+                    controller: "HomeController"
+                })
+                .when("/profile", {
+                    templateUrl: "views/users/profile.view.html",
+                    controller: "ProfileController"
+                })
+                .when("/admin", {
+                    templateUrl: "views/admin/admin.view.html",
+                    controller: "AdminController"
+                })
+                .otherwise({
+                    redirectTo: "/"
+                });
+        })
+        .controller("MainController", function($scope, $location){
+            $scope.$location = $location;
+        });
+})();
